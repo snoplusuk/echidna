bac42c44f7dd0d8ce3e6a495125caccc5ae10baa
Evelina.Arushanova <e.arushanova@qmul.ac.uk>
2015-02-04 17:02:19 +0000

    added filling mc spectra

--- echidna/core/fill_spectrum.py
+++ echidna/core/fill_spectrum.py
@@ -37,7 +37,7 @@ def _av_weights(times, T):
         weights.append( 1.0 )
     return (weights)
 
-def fill_spectrum(filename, spectrumname, T):
+def fill_reco_spectrum(filename, spectrumname, T):
     """This function fills in the ndarray of energies, radii, times 
     and weights. It takes the reconstructed energies and positions
     of the events from the root file. In order to keep the statistics, 
@@ -82,6 +82,61 @@ def fill_spectrum(filename, spectrumname, T):
 
         for time,weight in zip(times, weights):
             spectrum.fill(vertex.GetEnergy(), vertex.GetPosition().Mag(), time, weight)
+
+    return spectrum
+    
+
+def fill_mc_spectrum(filename, spectrumname, T):
+    """This function fills in the ndarray of true energies, radii, times 
+    and weights. It takes the true energies and positions of the events
+    from the root file. In order to keep the statistics, the time
+    dependence is performed via adding weights to every event depending 
+    on the time period. Both, studied time and Half-life must  be
+    written in the same units.  
+    
+    Args:
+      filename (str): A root file to study 
+      spectrumname (str): A name of future ndarray 
+      T (float): The Half-life of a studied background
+
+      Returns:
+        spectrum (:class:`echidna.core.spectra.Spectra`) 
+    """
+    print filename
+    print spectrumname
+    spectrum = spectra.Spectra(str(spectrumname))
+
+    times = [0]
+    for time_step in range(0, spectrum._time_bins):
+        time = time_step * spectrum._time_width + spectrum._time_low 
+        times.append(time)
+
+    if 'AV' in spectrumname:
+        print "AV WEIGHTS ARE CURRENTLY UNAVAILABLE"
+        weights = _av_weights(times, T)
+    else:
+        weights = _scint_weights(times, T)
+
+    for ds, run in rat.dsreader(filename):
+        if ds.GetEVCount() == 0:
+            continue
+        ev = ds.GetEV(0) 
+        mc = ds.GetMC()
+        particle_count = mc.GetMCParticleCount()
+        for iparticle in range(0, particle_count):
+            the_particle = mc.GetMCParticle(iparticle)
+            magnitude = the_particle.GetPosition().Mag()
+
+        vertex = ev.GetFitResult("scintFitter").GetVertex(0)
+        if not vertex.ContainsEnergy() or not vertex.ValidEnergy():
+            continue
+        if not vertex.ContainsPosition() or not vertex.ValidPosition():
+            continue
+        if vertex.GetPosition().Mag() > 3500.0:
+            continue
+
+        for time,weight in zip(times, weights):
+            spectrum.fill(mc.GetScintEnergyDeposit(), magnitude, time, weight)
 
     return spectrum
     
