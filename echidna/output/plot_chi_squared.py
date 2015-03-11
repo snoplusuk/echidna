dba622d8276f4b15b90f7184460a67df8902eee4
ashleyrback <ashleyrback@gmail.com>
2015-03-11 16:49:37 -0400

    Adds missing parts

--- echidna/output/plot_chi_squared.py
+++ echidna/output/plot_chi_squared.py
@@ -22,8 +22,10 @@ def chi_squared_vs_signal(signal_config, **kwargs):
     figure = pylab.figure()
     axis = figure.add_subplot(1, 1, 1)
     effective_masses = numpy.zeros(shape=(signal_config._chi_squareds[2].shape))
+    te130_coverter = decay.DBIsotope("Te130", 0.003, 129.906229, 127.6, 0.3408, 3.69e-14, 4.03, 1.269)
+    n_atoms = te130_converter.get_n_atoms()
     for i_bin, count in enumerate(signal_config._chi_squareds[2]):
-        effective_mass = decay.
+        effective_mass = decay.counts_to_mass(count, n_atoms)
         effective_masses[i_bin] = effective_mass
     x = effective_masses
     pylab.xlabel("Signal counts")
