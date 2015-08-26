500fb85588c8af1bf0c5919cdfe00333b1e51de1
James Waterfield <jw419@sussex.ac.uk>
2015-08-26 12:58:16 +0100

    Added fiducial volume cut to all extractors

--- echidna/core/dsextract.py
+++ echidna/core/dsextract.py
@@ -19,15 +19,15 @@ def function_factory(dimension, **kwargs):
       Extractor object.
     '''
     if dimension == "energy_mc":
-        return EnergyExtractMC()
+        return EnergyExtractMC(**kwargs)
     elif dimension == "energy_reco":
-        return EnergyExtractReco()
+        return EnergyExtractReco(**kwargs)
     elif dimension == "energy_truth":
-        return EnergyExtractTruth()
+        return EnergyExtractTruth(**kwargs)
     elif dimension == "radial_mc":
-        return RadialExtractMC()
+        return RadialExtractMC(**kwargs)
     elif dimension == "radial_reco":
-        return RadialExtractReco()
+        return RadialExtractReco(**kwargs)
     elif dimension == "radial3_mc":
         return Radial3ExtractMC(**kwargs)
     elif dimension == "radial3_reco":
@@ -41,44 +41,60 @@ class Extractor(object):
 
     Args:
       name (str): of the dimension
+      fid_r (float): Fiducial radius. Applies a cut to remove
+        events which have a radial position greater than the radius of the
+        fiducial volume. If None no cut is applied.
 
     Attributes:
       _name (str): of the dimension
+      _fid_r (float): Fiducial radius. Applies a cut to remove
+        events which have a radial position greater than the radius of the
+        fiducial volume. If None no cut is applied.
     '''
 
-    def __init__(self, name):
+    def __init__(self, name, fid_r):
         '''Initialise the class
         '''
-        self.name = name
+        self._name = name
+        self._fid_r = fid_r
 
 
 class EnergyExtractMC(Extractor):
     '''Quenched energy extraction methods.
+
+    Args:
+      fid_r (float, optional): Fiducial radius. Applies a cut to remove
+        events which have a radial position greater than the radius of the
+        fiducial volume. If None no cut is applied.
     '''
 
-    def __init__(self):
+    def __init__(self, fid_r=None):
         '''Initialise the class
         '''
-        super(EnergyExtractMC, self).__init__("energy_mc")
+        super(EnergyExtractMC, self).__init__("energy_mc", fid_r)
 
     def get_valid_root(self, mc):
         '''Check whether energy of a DS::MC is valid
 
         Args:
-          mc (:class:`RAT.DS.MC`) entry
+          mc (:class:`RAT.DS.MC`): entry
 
         Returns:
           bool: Validity boolean
         '''
         if mc.GetMCParticleCount > 0:
-            return True
-        return False
+            if self._fid_r:
+                if mc.GetMCParticle(0).GetPosition().Mag() < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            return True  # Passes particle count and no fid v cut
+        return False  # Fails particle count
 
     def get_value_root(self, mc):
         '''Get energy value from a DS::MC
 
         Args:
-          mc (:class:`RAT.DS.MC`) entry
+          mc (:class:`RAT.DS.MC`): entry
 
         Returns:
           float: True quenched energy
@@ -95,8 +111,15 @@ class EnergyExtractMC(Extractor):
           bool: Validity boolean
         '''
         if entry.mc == 1:
+            if self._fid_r:
+                if numpy.fabs(numpy.sqrt((entry.mcPosx)**2 +
+                                         (entry.mcPosy)**2 +
+                                         (entry.mcPosz)**2)) < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            # MC has associated trigger or vice versa and no fid v applied
             return True
-        return False
+        return False  # No associated trigger with mc or vice versa
 
     def get_value_ntuple(self, entry):
         '''Get energy value from an ntuple MC
@@ -112,12 +135,17 @@ class EnergyExtractMC(Extractor):
 
 class EnergyExtractReco(Extractor):
     '''Reconstructed energy extraction methods.
+
+    Args:
+      fid_r (float, optional): Fiducial radius. Applies a cut to remove
+        events which have a radial position greater than the radius of the
+        fiducial volume. If None no cut is applied.
     '''
 
-    def __init__(self):
+    def __init__(self, fid_r=None):
         '''Initialise the class
         '''
-        super(EnergyExtractReco, self).__init__("energy_reco")
+        super(EnergyExtractReco, self).__init__("energy_reco", fid_r)
 
     def get_valid_root(self, ev):
         '''Check whether energy of a DS::EV is valid
@@ -131,8 +159,12 @@ class EnergyExtractReco(Extractor):
         if ev.DefaultFitVertexExists() and \
                 ev.GetDefaultFitVertex().ContainsEnergy() \
                 and ev.GetDefaultFitVertex().ValidEnergy():
-            return True
-        return False
+            if self._fid_r:
+                if ev.GetDefaultFitVertex().GetPosition().Mag() < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            return True  # Passes fit checks and no fid v cut
+        return False  # Fails fit checks
 
     def get_value_root(self, ev):
         '''Get energy value from a DS::EV
@@ -154,7 +186,15 @@ class EnergyExtractReco(Extractor):
         Returns:
           bool: Validity boolean
         '''
-        return (entry.scintFit != 0 and entry.energy > 0)
+        if entry.scintFit == 1 and entry.energy > 0:
+            if self._fid_r:
+                if numpy.fabs(numpy.sqrt((entry.posx)**2 +
+                                         (entry.posy)**2 +
+                                         (entry.posz)**2)) < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            return True  # Passes scintFit, has energy and no fid v cut
+        return False  # Fails scintFit or no energy
 
     def get_value_ntuple(self, entry):
         '''Get energy value from an ntuple EV
@@ -170,12 +210,17 @@ class EnergyExtractReco(Extractor):
 
 class EnergyExtractTruth(Extractor):
     '''True MC energy extraction methods.
+
+    Args:
+      fid_r (float, optional): Fiducial radius. Applies a cut to remove
+        events which have a radial position greater than the radius of the
+        fiducial volume. If None no cut is applied.
     '''
 
-    def __init__(self):
+    def __init__(self, fid_r=None):
         '''Initialise the class
         '''
-        super(EnergyExtractTruth, self).__init__("energy_truth")
+        super(EnergyExtractTruth, self).__init__("energy_truth", fid_r)
 
     def get_valid_root(self, mc):
         '''Check whether energy of a DS::MC is valid
@@ -187,8 +232,12 @@ class EnergyExtractTruth(Extractor):
           bool: Validity boolean
         '''
         if mc.GetMCParticleCount > 0:
-            return True
-        return False
+            if self._fid_r:
+                if mc.GetMCParticle(0).GetPosition().Mag() < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            return True  # Passes particle count and no fid v cut
+        return False  # Fails particle count
 
     def get_value_root(self, mc):
         '''Get energy value from a DS::MC
@@ -211,8 +260,15 @@ class EnergyExtractTruth(Extractor):
           bool: Validity boolean
         '''
         if entry.mc == 1:
+            if self._fid_r:
+                if numpy.fabs(numpy.sqrt((entry.mcPosx)**2 +
+                                         (entry.mcPosy)**2 +
+                                         (entry.mcPosz)**2)) < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            # MC has associated trigger or vice versa and no fid v applied
             return True
-        return False
+        return False  # No associated trigger with mc or vice versa
 
     def get_value_ntuple(self, entry):
         '''Get energy value from an ntuple MC
@@ -228,12 +284,17 @@ class EnergyExtractTruth(Extractor):
 
 class RadialExtractMC(Extractor):
     '''True radial extraction methods.
+
+    Args:
+      fid_r (float, optional): Fiducial radius. Applies a cut to remove
+        events which have a radial position greater than the radius of the
+        fiducial volume. If None no cut is applied.
     '''
 
-    def __init__(self):
+    def __init__(self, fid_r=None):
         '''Initialise the class
         '''
-        super(RadialExtractMC, self).__init__("radial_mc")
+        super(RadialExtractMC, self).__init__("radial_mc", fid_r)
 
     def get_valid_root(self, mc):
         '''Check whether radius of a DS::MC is valid
@@ -245,8 +306,12 @@ class RadialExtractMC(Extractor):
           bool: Validity boolean
         '''
         if mc.GetMCParticleCount > 0:
-            return True
-        return False
+            if self._fid_r:
+                if mc.GetMCParticle(0).GetPosition().Mag() < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            return True  # Passes particle count and no fid v cut
+        return False  # Fails particle count
 
     def get_value_root(self, mc):
         '''Get radius value from a DS::MC
@@ -269,8 +334,15 @@ class RadialExtractMC(Extractor):
           bool: Validity boolean
         '''
         if entry.mc == 1:
+            if self._fid_r:
+                if numpy.fabs(numpy.sqrt((entry.mcPosx)**2 +
+                                         (entry.mcPosy)**2 +
+                                         (entry.mcPosz)**2)) < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            # MC has associated trigger or vice versa and no fid v applied
             return True
-        return False
+        return False  # No associated trigger with mc or vice versa
 
     def get_value_ntuple(self, entry):
         '''Get radius value from an ntuple MC
@@ -288,12 +360,17 @@ class RadialExtractMC(Extractor):
 
 class RadialExtractReco(Extractor):
     '''Reconstructed radial extraction methods.
+
+    Args:
+      fid_r (float, optional): Fiducial radius. Applies a cut to remove
+        events which have a radial position greater than the radius of the
+        fiducial volume. If None no cut is applied.
     '''
 
-    def __init__(self):
+    def __init__(self, fid_r=None):
         '''Initialise the class
         '''
-        super(RadialExtractReco, self).__init__("radial_reco")
+        super(RadialExtractReco, self).__init__("radial_reco", fid_r)
 
     def get_valid_root(self, ev):
         '''Check whether radius of a DS::EV is valid
@@ -330,7 +407,15 @@ class RadialExtractReco(Extractor):
         Returns:
           bool: Validity boolean
         '''
-        return entry.scintFit != 0
+        if entry.scintFit == 1:
+            if self._fid_r:
+                if numpy.fabs(numpy.sqrt((entry.posx)**2 +
+                                         (entry.posy)**2 +
+                                         (entry.posz)**2)) < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            return True  # Passes scintFit and no fid v cut
+        return False  # Fails scintFit
 
     def get_value_ntuple(self, entry):
         '''Get radius value from an ntuple EV
@@ -348,12 +433,24 @@ class RadialExtractReco(Extractor):
 
 class Radial3ExtractMC(Extractor):
     ''' True :math:`(radius/av_radius)^3`radial extraction methods.
+
+    Args:
+      fid_r (float, optional): Fiducial radius. Applies a cut to remove
+        events which have a radial position greater than the radius of the
+        fiducial volume. If None no cut is applied.
+      av_radius (float, optional): The radius of the AV used in calculating
+        :math:`(radius/av_radius)^3`. If None then the av_radius in
+        :class:`echidna.calc.constants` is used in the calculation.
+
+    Attributes:
+      _av_radius (float, optional): The radius of the AV used in calculating
+        :math:`(radius/av_radius)^3`.
     '''
 
-    def __init__(self, av_radius=None):
+    def __init__(self, fid_r=None, av_radius=None):
         '''Initialise the class
         '''
-        super(Radial3ExtractMC, self).__init__("radial3_mc")
+        super(Radial3ExtractMC, self).__init__("radial3_mc", fid_r)
         if av_radius:
             self._av_radius = av_radius
         else:
@@ -369,8 +466,12 @@ class Radial3ExtractMC(Extractor):
           bool: Validity boolean
         '''
         if mc.GetMCParticleCount > 0:
-            return True
-        return False
+            if self._fid_r:
+                if mc.GetMCParticle(0).GetPosition().Mag() < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            return True  # Passes particle count and no fid v cut
+        return False  # Fails particle count
 
     def get_value_root(self, mc):
         '''Get radius value from a DS::MC
@@ -393,8 +494,15 @@ class Radial3ExtractMC(Extractor):
           bool: Validity boolean
         '''
         if entry.mc == 1:
+            if self._fid_r:
+                if numpy.fabs(numpy.sqrt((entry.mcPosx)**2 +
+                                         (entry.mcPosy)**2 +
+                                         (entry.mcPosz)**2)) < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            # MC has associated trigger or vice versa and no fid v applied
             return True
-        return False
+        return False  # No associated trigger with mc or vice versa
 
     def get_value_ntuple(self, entry):
         '''Get radius value from an ntuple MC
@@ -413,12 +521,24 @@ class Radial3ExtractMC(Extractor):
 
 class Radial3ExtractReco(Extractor):
     ''' Reconstructed :math:`(radius/av_radius)^3` radial extraction methods.
+
+    Args:
+      fid_r (float, optional): Fiducial radius. Applies a cut to remove
+        events which have a radial position greater than the radius of the
+        fiducial volume. If None no cut is applied.
+      av_radius (float, optional): The radius of the AV used in calculating
+        :math:`(radius/av_radius)^3`. If None then the av_radius in
+        :class:`echidna.calc.constants` is used in the calculation.
+
+    Attributes:
+      _av_radius (float, optional): The radius of the AV used in calculating
+        :math:`(radius/av_radius)^3`.
     '''
 
-    def __init__(self, av_radius=None):
+    def __init__(self, fid_r=None, av_radius=None):
         '''Initialise the class
         '''
-        super(Radial3ExtractReco, self).__init__("radial3_reco")
+        super(Radial3ExtractReco, self).__init__("radial3_reco", fid_r)
         if av_radius:
             self._av_radius = av_radius
         else:
@@ -434,10 +554,14 @@ class Radial3ExtractReco(Extractor):
           bool: Validity boolean
         '''
         if ev.DefaultFitVertexExists() and \
-                ev.GetDefaultFitVertex().ContainsPosition() \
-                and ev.GetDefaultFitVertex().ValidPosition():
-            return True
-        return False
+                ev.GetDefaultFitVertex().ContainsEnergy() \
+                and ev.GetDefaultFitVertex().ValidEnergy():
+            if self._fid_r:
+                if ev.GetDefaultFitVertex().GetPosition().Mag() < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            return True  # Passes fit checks and no fid v cut
+        return False  # Fails fit checks
 
     def get_value_root(self, ev):
         '''Get radius value from a DS::EV
@@ -460,7 +584,15 @@ class Radial3ExtractReco(Extractor):
         Returns:
           bool: Validity boolean
         '''
-        return entry.scintFit != 0
+        if entry.scintFit == 1:
+            if self._fid_r:
+                if numpy.fabs(numpy.sqrt((entry.posx)**2 +
+                                         (entry.posy)**2 +
+                                         (entry.posz)**2)) < self._fid_r:
+                    return True  # Passes fiducial volume cut
+                return False  # Fails fiducial volume cut
+            return True  # Passes scintFit and no fid v cut
+        return False  # Fails scintFit
 
     def get_value_ntuple(self, entry):
         '''Get radius value from an ntuple EV
